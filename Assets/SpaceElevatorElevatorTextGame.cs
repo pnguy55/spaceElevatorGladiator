4c84b1e210a36329e0788cf6071381ff48db1da0
Pnguy55 <pnguy55@gmail.com>
2018-06-08 17:31:45 -0500

    Initial commit.

---
+++ Assets/SpaceElevatorElevatorTextGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceElevatorElevatorTextGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+	// Use this for initialization
+	void Start () {
+		textComponent.text = ("Yo Yo Yo who it be ?");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
